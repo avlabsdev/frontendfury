5a41d9a6ebe289c4e043a870d3f3d71b22cf686f
Aaron Varga <aaronvarga@myyahoo.com>
2024-11-05 19:06:07 -0500

    header update, added photo

--- my-app/src/app/components/header.tsx
+++ my-app/src/app/components/header.tsx
@@ -1,12 +1,15 @@
-// import Image from "next/image";
+import Image from "next/image";
 
 import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between">
-      <Link href="/" className="font-bold text-lg">
-        frontEndFury
+      <Link href="/" className="font-bold text-lg flex items-center gap-2">
+        <Image src="/fef-logo.webp" alt="frontEndFury" width={32} height={32} />
+        <span className="bg-gradient-to-r from-yellow-200 to-orange-500 bg-clip-text text-transparent mt-1">
+          frontEndFury
+        </span>
       </Link>
       <nav className="flex gap-8 items-center">
         <Link href="/courses" className="underline hover:no-underline">
