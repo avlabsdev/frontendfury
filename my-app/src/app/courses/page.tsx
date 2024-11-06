ae2214ca39002f63581f800b640f23429892cdab
Aaron Varga <aaronvarga@myyahoo.com>
2024-11-06 11:16:45 -0500

    Delete my-app directory

--- my-app/src/app/courses/page.tsx
+++
@@ -1,7 +0,0 @@
-// import Image from "next/image";
-
-// import Link from "next/link";
-
-export default function Courses() {
-  return <div className="container px-8">Courses</div>;
-}
--- my-app/src/app/resources/page.tsx
+++
@@ -1,7 +0,0 @@
-// import Image from "next/image";
-
-// import Link from "next/link";
-
-export default function Resources() {
-  return <div className="container px-8">Resources</div>;
-}
